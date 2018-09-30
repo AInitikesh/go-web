fdb4ca8fdb89e189a0be9ce067d9fba3ee880b3f
Nitikesh <er.nitikeshbhad@gmail.com>
2018-09-30 12:32:45 +0530

    add template

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 		fmt.Fprint(w, "Hello Go Web Development")
 	})
 
