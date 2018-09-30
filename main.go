5c52df4f0f48831cef378e30f95ea057d4504ff3
Nitikesh <er.nitikeshbhad@gmail.com>
2018-09-30 13:52:54 +0530

    add books table

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -12,6 +13,13 @@ import (
 type Page struct {
 	Name     string
 	DBStatus bool
+}
+
+type SearchResult struct {
+	Title  string
+	Author string
+	Year   string
+	ID     string
 }
 
 func main() {
@@ -29,6 +37,19 @@ func main() {
 		p.DBStatus = db.Ping() == nil
 
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		results := []SearchResult{
+			SearchResult{"Moby-DIck", "Herman Melville", "1851", "2222222"},
+			SearchResult{"The Adventures of Huckleberry Finn", "Mark Twain", "1884", "44444444"},
+			SearchResult{"The Catch of Rye", "JD Salinger", "1951", "333333"},
+		}
+
+		encoder := json.NewEncoder(w)
+		if err := encoder.Encode(results); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
